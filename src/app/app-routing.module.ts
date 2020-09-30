5ae6d873a9dbbeb3826d912937a7af886bc9bcff
Hugo Fucksmann <hugoffuksmann@gmail.com>
2020-09-30 00:05:51 -0300

    correccion de errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'inicio', component: InicioComponent},
-  { path: 'goty', component: GotyComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+  { path: 'Arg', component: InicioComponent},
+  { path: 'Mundo', component: GotyComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'Arg'}
 ]
 
 @NgModule({
